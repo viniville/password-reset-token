cb5d2f2aee3d40126f88160f6c062657d1add7d5
Vinicius Ville <viniville@gmail.com>
2023-08-17 16:14:16 -0300

    feat: upgrade spring security config

--- src/main/java/com/poc/passwordresettoken/config/security/SecurityConfig.java
+++ src/main/java/com/poc/passwordresettoken/config/security/SecurityConfig.java
@@ -5,10 +5,14 @@ import lombok.SneakyThrows;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @RequiredArgsConstructor
 @Configuration
@@ -18,14 +22,20 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) {
         return http
-                .httpBasic()
-                .and()
-                .authorizeHttpRequests()
-                .requestMatchers(HttpMethod.POST, "/user/forgotPassword").permitAll()
-                .requestMatchers(HttpMethod.POST, "/user/resetPassword").permitAll()
-                .anyRequest().authenticated()//.permitAll()
-                .and()
-                .csrf().disable()
+                .httpBasic(Customizer.withDefaults())
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(smc -> smc.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(amrmr -> amrmr
+                        .requestMatchers(
+                                new AntPathRequestMatcher("/user/forgotPassword", HttpMethod.POST.name(), false),
+                                new AntPathRequestMatcher("/user/resetPassword", HttpMethod.POST.name(), false),
+                                new AntPathRequestMatcher("/swagger-ui.html", HttpMethod.GET.name(), false),
+                                new AntPathRequestMatcher("/swagger-ui/*", HttpMethod.GET.name(), false),
+                                new AntPathRequestMatcher("/v3/api-docs/swagger-config", HttpMethod.GET.name(), false),
+                                new AntPathRequestMatcher("/v3/api-docs.yaml", HttpMethod.GET.name(), false),
+                                new AntPathRequestMatcher("/v3/api-docs", HttpMethod.GET.name(), false)
+                        ).permitAll()
+                        .anyRequest().authenticated())
                 .build();
     }
 
